21db434dd819accaee24d4317bfa6746f8cc2121
Larisasmiles <larisasmiles@gmail.com>
2018-02-16 12:25:04 -0700

    routing for html

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,16 @@
+var path = require("path");
+
+///////////////////////////////////////
+// Routing for html files
+//////////////////////////////////////
+
+module.exports = function(app) {
+	// route for survey 
+	app.get("/survey", function(req, res) {
+		res.sendFile(path.join(__dirname, "../public/survey.html"));
+	});
+	// if route doesnt match then default to home
+	app.get("*", function(req, res) {
+		res.sendFile(path.join(__dirname, "../public/home.html"));
+	});
+};
