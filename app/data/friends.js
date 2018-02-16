60678e33d14cb1af0f857ef4b83312c1a1ac59f6
Larisasmiles <larisasmiles@gmail.com>
2018-02-16 12:18:49 -0700

    added friends with pictures

---
+++ app/data/friends.js
@@ -0,0 +1,115 @@
+let friendsArray = [{
+  name: 'Rachel Green',
+  photo: 'https://a1cf74336522e87f135f-2f21ace9a6cf0052456644b80fa06d4f.ssl.cf2.rackcdn.com/images/characters/p-friends-jennifer-aniston.jpg',
+  scores: [
+      '1',
+      '5',
+      '5',
+      '1',
+      '5',
+      '5',
+      '1',
+      '1',
+      '1',
+      '5'
+  ]
+},
+{
+  name: 'Monica Geller',
+  photo: 'https://upload.wikimedia.org/wikipedia/en/d/d0/Courteney_Cox_as_Monica_Geller.jpg',
+  scores: [
+      '2',
+      '4',
+      '1',
+      '1',
+      '1',
+      '1',
+      '1',
+      '2',
+      '1',
+      '5'
+  ]
+},
+{
+  name: 'Joey Tribbiani',
+  photo: 'https://upload.wikimedia.org/wikipedia/en/thumb/d/da/Matt_LeBlanc_as_Joey_Tribbiani.jpg/240px-Matt_LeBlanc_as_Joey_Tribbiani.jpg',
+  scores: [
+      '4',
+      '3',
+      '5',
+      '1',
+      '1',
+      '5',
+      '5',
+      '1',
+      '1',
+      '1'
+  ]
+},
+{
+  name: 'Chandler Bing',
+  photo: 'https://pixel.nymag.com/imgs/daily/vulture/2017/03/30/30-chandler-bing.w190.h190.2x.jpg',
+  scores: [
+      '5',
+      '1',
+      '2',
+      '1',
+      '3',
+      '4',
+      '1',
+      '3',
+      '3',
+      '4'
+  ]
+},
+{
+  name: 'Phoebe Buffay',
+  photo: 'https://upload.wikimedia.org/wikipedia/en/f/f6/Friendsphoebe.jpg',
+  scores: [
+      '1',
+      '1',
+      '5',
+      '5',
+      '5',
+      '5',
+      '5',
+      '5',
+      '5',
+      '1'
+  ]
+},
+{
+  name: 'Ross Geller',
+  photo: 'https://vignette.wikia.nocookie.net/friends/images/0/0b/RossGeller.jpg',
+  scores: [
+      '1',
+      '1',
+      '5',
+      '1',
+      '3',
+      '2',
+      '5',
+      '1',
+      '1',
+      '5'
+  ]
+},
+{
+  name: 'Janice',
+  photo: 'http://1.bp.blogspot.com/-1rz5KxncouM/VSqQNEJYwkI/AAAAAAAABsk/elkIQ3NsMuw/s1600/janice-from-friends-34854473-500-564.jpg',
+  scores: [
+      '5',
+      '5',
+      '5',
+      '5',
+      '5',
+      '5',
+      '5',
+      '5',
+      '5',
+      '5'
+  ]
+}
+];
+
+module.exports = friendsArray;
